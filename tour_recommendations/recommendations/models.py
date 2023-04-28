b7c4ec1e364b2b3465f78934e91031db840eaa3e
Максим Авдошин <Zlyden-1@yandex.ru>
2023-04-28 23:20:40 +0400

    added user profile

--- tour_recommendations/recommendations/models.py
+++ tour_recommendations/recommendations/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    categories = models.ManyToManyField(to='Category')
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/',
+                              blank=True)
+
+    def __str__(self):
+        return f'Profile of {self.user.username}'
+
 
 class Events(models.Model):
     title = models.CharField(max_length=500)
@@ -11,6 +23,7 @@ class Events(models.Model):
     categories = models.ManyToManyField(to='Category')
     description = models.TextField()
     image = models.ImageField()
+
 
 class Category(models.Model):
     title = models.CharField(max_length=200)

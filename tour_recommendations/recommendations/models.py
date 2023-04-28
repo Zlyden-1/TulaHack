a4123bf29d23c207abf24b27565df3c12c862eaf
DashulkaUbivator <doratraveled01@gmail.com>
2023-04-28 22:31:31 +0400

    models

--- tour_recommendations/recommendations/models.py
+++ tour_recommendations/recommendations/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Events(models.Model):
+    title = models.CharField(max_length=500)
+    city = models.CharField(max_length=200)
+    location = models.CharField(max_length=300)
+    date = models.DateTimeField()
+    end_reg = models.DateTimeField()
+    categories = models.ManyToManyField(to='Category')
+    description = models.TextField()
+    image = models.ImageField()
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)

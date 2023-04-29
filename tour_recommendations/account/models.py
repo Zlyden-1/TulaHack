6c87501184315ca4cd4807d5c6fdfb3210a01460
Максим Авдошин <Zlyden-1@yandex.ru>
2023-04-29 11:18:07 +0400

    added basic registration

--- tour_recommendations/account/models.py
+++ tour_recommendations/account/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.conf import settings
 
+from recommendations.models import Category
+
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
-    categories = models.ManyToManyField(to='Category')
+    categories = models.ManyToManyField(to=Category, null=True, blank=True)
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d/',
                               blank=True)
--- tour_recommendations/recommendations/views.py
+++ tour_recommendations/recommendations/views.py
@@ -1,29 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-
-from tour_recommendations.account.forms import LoginForm
-
-
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Вход произведен успешно!')
-                else:
-                    return HttpResponse('Аккаунт неактивен!')
-            else:
-                return HttpResponse('Пароль и/или логин неверны!')
-    else:
-        form = LoginForm()
-        return render(request, 'account/login.html', {'form': form})
 
 
 def index(request):
